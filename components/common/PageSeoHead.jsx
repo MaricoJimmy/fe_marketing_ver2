5695c757c8b1ea9c152bab6bb1522706bb6a0e0b
Tran Thu Thao <tranthuthao.bhn.96@gmail.com>
2023-01-13 22:07:54 +0700

    add meta desc

--- components/common/PageSeoHead.jsx
+++ components/common/PageSeoHead.jsx
@@ -11,7 +11,7 @@ function PageSeoHead({ data }) {
                     property="og:description"
                     content={data.desc}
                 ></meta>
-                <meta name="description" content={data.desc} />
+                <meta name="description" content={data.desc}></meta>
                 <meta property="og:image" content={data.img}></meta>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
