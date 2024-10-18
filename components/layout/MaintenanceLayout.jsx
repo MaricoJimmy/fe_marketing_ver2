54ced90621d00a5344d40cb2fa41146ac8cc9147
thaoktk <tranthuthao.bhn.96@gmail.com>
2024-10-18 09:42:51 +0700

    change UI of maintenance page

--- components/layout/MaintenanceLayout.jsx
+++ components/layout/MaintenanceLayout.jsx
@@ -16,30 +16,18 @@ function MaintenanceLayout({ children }) {
       <>
         <PageSeoHead data={metaData} />
         <div className="p-6 w-screen h-screen flex flex-col items-center justify-center">
-          <div>
-            <Image
-              src="/image/logo/logo_1.png"
-              width="150"
-              height="40"
-              alt=""
-            />
-          </div>
           <div className="flex-1 flex flex-col justify-center items-center">
             <div>
               <Image
-                src="/image/coming-soon.svg"
+                src="/image/logo/logo.svg"
                 width="500"
-                height="300"
+                height="120"
                 alt=""
               />
             </div>
-            <h1 className="lg:text-6xl text-5xl text-center bg-gradient-to-br from-[#02D2DA] to-[#0B32EF] text-transparent bg-clip-text !leading-snug font-bold">
-              Coming soon
+            <h1 className="mt-2 md:text-2xl text-lg text-center text-gray font-semibold">
+              See you in November.
             </h1>
-            <p className="mt-2 lg:max-w-[450px] text-base text-gray font-medium text-center">
-              Website hiện đang trong giai đoạn phát triển. Chúng tôi sẽ trở lại
-              vào tháng 11!
-            </p>
           </div>
         </div>
       </>
