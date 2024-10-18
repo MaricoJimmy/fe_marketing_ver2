7637298117eebe201b050f5c39164641fa93d759
thaoktk <tranthuthao.bhn.96@gmail.com>
2024-10-18 10:23:37 +0700

    add gg analytics id

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-TF6YXCZ2TW"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
