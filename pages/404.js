3142530a290612547ec86298ee12892ea8c86c52
thaoktk <tranthuthao.bhn.96@gmail.com>
2024-11-12 16:05:21 +0700

    fix title of 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,26 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from "next/head";
+import React from "react";
 
 function NotFound() {
-    return (
-        <>
-            <Head>
-                <title>404 | Pambu</title>
-                <meta property="og:title" content="404"></meta>
-                <meta property="og:description" content="Trang này không tồn tại"></meta>
-                <meta property="og:image" content="/image/logo.png"></meta>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            <div className='min-h-screen flex items-center justify-center'>
-                <p className='text-xl text-center font-semibold'>404 | Trang này không tồn tại.</p>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>404 | Udata.ai</title>
+        <meta property="og:title" content="404"></meta>
+        <meta
+          property="og:description"
+          content="Trang này không tồn tại"
+        ></meta>
+        <meta property="og:image" content="/image/logo.png"></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-xl text-center font-semibold">
+          404 | Trang này không tồn tại.
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
