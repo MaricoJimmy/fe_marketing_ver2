b042f36c513ef41158107967a6f4727d21fbeb34
thaott-udata <thaott@udata.ai>
2025-07-10 09:33:18 +0700

    fix: update LinkedIn link text from 'Udata.dhg' to 'udata.jsc'

--- pages/dung-thu.js
+++ pages/dung-thu.js
@@ -374,7 +374,7 @@ const ContactPage = () => {
                           href="https://www.linkedin.com/company/udatadhg"
                           className="text-neutral font-medium"
                         >
-                          Udata.dhg
+                          udata.jsc
                         </a>
                       </div>
                     </div>
