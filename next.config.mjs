4c4fa7e8f203b7760c114791132a4593529c2033
MaricoJimmy <vuvantung204@gmail.com>
2026-07-01 22:27:31 +0700

    hotfix: add SEO redirects from old routes to new routes

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ const nextConfig = {
   devIndicators: false,
   output: 'standalone',
   allowedDevOrigins: ['192.168.1.138'],
+  async redirects() {
+    return [
+      {
+        source: '/ve-cong-ty',
+        destination: '/about-us',
+        permanent: true, // Trả về mã 308 (Tốt cho SEO - Google sẽ tự cập nhật link)
+      },
+      {
+        source: '/tuyen-dung',
+        destination: '/careers',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
